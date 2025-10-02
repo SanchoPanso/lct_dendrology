dfa05afc68f958b0995828239e25d0a683e66f77
SanchoPanso <20alexchernik@gmail.com>
2025-10-02 23:40:28 +0300

    feat: update downloaded models

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -2,5 +2,5 @@
 TARGET_DIR="$(dirname "$0")/../models"
 
 mkdir -p "$TARGET_DIR"
-wget -O "$TARGET_DIR/species_classifier_v1.pt" "https://github.com/SanchoPanso/lct_dendrology/releases/download/v0.2.0/species_classifier_v1.pt"
-wget -O "$TARGET_DIR/tree_detector_v2.pt" "https://github.com/SanchoPanso/lct_dendrology/releases/download/v0.2.0/tree_detector_v2.pt"
+wget -O "$TARGET_DIR/species_classifier_v2.pt" "https://github.com/SanchoPanso/lct_dendrology/releases/download/v0.3.0/species_classifier_v2.pt"
+wget -O "$TARGET_DIR/tree_detector_v2.pt" "https://github.com/SanchoPanso/lct_dendrology/releases/download/v0.3.0/tree_detector_v2.pt"
